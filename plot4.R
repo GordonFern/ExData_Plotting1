17291143f0b33b10c7608144efdc1c801ecc0569
GordonFern <gordon.fern@btinternet.com>
2015-04-11 23:04:09 +0100

    Legend didn't start with capital letter

--- plot4.R
+++ plot4.R
@@ -103,7 +103,7 @@ plotSubMetering<-function(x) {
   lines(x$Date_Time,x$Sub_metering_3,col="blue")
   d<-seq(as.POSIXct("2007-02-01"),as.POSIXct("2007-02-03"),by="day")
   axis.POSIXct(1,at=d,format="%a")
-  legend("topright",c("sub_metering_1","sub_metering_2","sub_metering_3"),
+  legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
          lty=c(1,1),col=c("black","red","blue"),bty="n")
   
 }
